bcc4a91a411f1fd686526bdd466c9fe287ed9729
Johnson Liu <jyliu17@gmail.com>
2020-09-06 02:47:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -70,8 +70,27 @@ def game_hash
       :colors => ["Turquiose, Purple"],
       :players => [
         {
-          
-        }
+          :player_name => "Jeff Adrien",
+          :number => 4, 
+          :shoe => 18,
+          :points => 10,
+          :rebounds => 1,
+          :assists => 1,
+          :steals => 2,
+          :blocks => 7,
+          :slam_dunks => 2 
+        },
+        {
+          :player_name => "Jeff Adrien",
+          :number => 4, 
+          :shoe => 18,
+          :points => 10,
+          :rebounds => 1,
+          :assists => 1,
+          :steals => 2,
+          :blocks => 7,
+          :slam_dunks => 2 
+        },
        ]
       }
      }
