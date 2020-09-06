72f4f16edb544bd1d1c1fff67c9482d5a268c207
Johnson Liu <jyliu17@gmail.com>
2020-09-06 02:54:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -113,6 +113,17 @@ def game_hash
           :blocks => 1,
           :slam_dunks => 0 
         },
+        {
+          :player_name => "Kemba Walker",
+          :number => 33, 
+          :shoe => 15,
+          :points => 6,
+          :rebounds => 12,
+          :assists => 21,
+          :steals => 7,
+          :blocks => 5,
+          :slam_dunks => 12 
+        },
        ]
       }
      }
